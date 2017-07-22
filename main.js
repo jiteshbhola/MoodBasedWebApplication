c59e549b5c9403f9d71bcfa46787507b66f84e8d
Jitesh <jbhola68@gmail.com>
2017-07-22 10:58:55 +0530

    last modifications made

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-$('.welcome-screen button').on('click', function()
+$('.welcome-screen button').on('click', function()   //dispaly name function
   {
       var name = $('#name-input').val();
         if (name.length > 2) {
            var message = "Welcome, " + name;
             $('.main .user-name').text(message);
             $('.welcome-screen').addClass('hidden');
-$('.main').removeClass('hidden');
+            $('.main').removeClass('hidden');
         } else {
            $('#name-input').addClass('error');
         }
  });
 
 
-$('body').on('keypress',function(event) 
+$('body').on('keypress',function(event)   //display name on keypress enter
 {
     if (event.keyCode == 13) //13 is keycode for ENTER
       {
